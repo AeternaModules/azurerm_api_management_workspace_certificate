0da1124c877ff605cc025ace501c100670bb6b38
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:49:31 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "api_management_workspace_certificates" {
+  description = "All api_management_workspace_certificate resources"
+  value       = azurerm_api_management_workspace_certificate.api_management_workspace_certificates
+}
+
