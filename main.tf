1d53984f8f9f38fb5f2ac18a2972ac70cf680a40
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:57:41 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,11 +1,21 @@
+data "azurerm_key_vault_secret" "certificate_data_base64" {
+  for_each     = { for k, v in var.api_management_workspace_certificates : k => v if v.certificate_data_base64_key_vault_id != null && v.certificate_data_base64_key_vault_secret_name != null }
+  name         = each.value.certificate_data_base64_key_vault_secret_name
+  key_vault_id = each.value.certificate_data_base64_key_vault_id
+}
+data "azurerm_key_vault_secret" "password" {
+  for_each     = { for k, v in var.api_management_workspace_certificates : k => v if v.password_key_vault_id != null && v.password_key_vault_secret_name != null }
+  name         = each.value.password_key_vault_secret_name
+  key_vault_id = each.value.password_key_vault_id
+}
 resource "azurerm_api_management_workspace_certificate" "api_management_workspace_certificates" {
   for_each = var.api_management_workspace_certificates
 
   api_management_workspace_id      = each.value.api_management_workspace_id
   name                             = each.value.name
-  certificate_data_base64          = each.value.certificate_data_base64
+  certificate_data_base64          = each.value.certificate_data_base64 != null ? each.value.certificate_data_base64 : try(data.azurerm_key_vault_secret.certificate_data_base64[each.key].value, null)
   key_vault_secret_id              = each.value.key_vault_secret_id
-  password                         = each.value.password
+  password                         = each.value.password != null ? each.value.password : try(data.azurerm_key_vault_secret.password[each.key].value, null)
   user_assigned_identity_client_id = each.value.user_assigned_identity_client_id
 }
 
--- variables.tf
+++ variables.tf
@@ -6,18 +6,26 @@ Required:
     - name
 Optional:
     - certificate_data_base64
+    - certificate_data_base64_key_vault_id (alternative to certificate_data_base64 - read from Key Vault instead)
+    - certificate_data_base64_key_vault_secret_name (alternative to certificate_data_base64 - read from Key Vault instead)
     - key_vault_secret_id
     - password
+    - password_key_vault_id (alternative to password - read from Key Vault instead)
+    - password_key_vault_secret_name (alternative to password - read from Key Vault instead)
     - user_assigned_identity_client_id
 EOT
 
   type = map(object({
-    api_management_workspace_id      = string
-    name                             = string
-    certificate_data_base64          = optional(string)
-    key_vault_secret_id              = optional(string)
-    password                         = optional(string)
-    user_assigned_identity_client_id = optional(string)
+    api_management_workspace_id                   = string
+    name                                          = string
+    certificate_data_base64                       = optional(string)
+    certificate_data_base64_key_vault_id          = optional(string)
+    certificate_data_base64_key_vault_secret_name = optional(string)
+    key_vault_secret_id                           = optional(string)
+    password                                      = optional(string)
+    password_key_vault_id                         = optional(string)
+    password_key_vault_secret_name                = optional(string)
+    user_assigned_identity_client_id              = optional(string)
   }))
 }
 
