61f2988dd33de06620676f4b3f9de10099dec75a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:41 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,44 +1,39 @@
-output "api_management_workspace_certificates" {
-  description = "All api_management_workspace_certificate resources"
-  value       = azurerm_api_management_workspace_certificate.api_management_workspace_certificates
-  sensitive   = true
-}
 output "api_management_workspace_certificates_api_management_workspace_id" {
-  description = "List of api_management_workspace_id values across all api_management_workspace_certificates"
-  value       = [for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : v.api_management_workspace_id]
+  description = "Map of api_management_workspace_id values across all api_management_workspace_certificates, keyed the same as var.api_management_workspace_certificates"
+  value       = { for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : k => v.api_management_workspace_id }
 }
 output "api_management_workspace_certificates_certificate_data_base64" {
-  description = "List of certificate_data_base64 values across all api_management_workspace_certificates"
-  value       = [for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : v.certificate_data_base64]
+  description = "Map of certificate_data_base64 values across all api_management_workspace_certificates, keyed the same as var.api_management_workspace_certificates"
+  value       = { for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : k => v.certificate_data_base64 }
   sensitive   = true
 }
 output "api_management_workspace_certificates_expiration" {
-  description = "List of expiration values across all api_management_workspace_certificates"
-  value       = [for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : v.expiration]
+  description = "Map of expiration values across all api_management_workspace_certificates, keyed the same as var.api_management_workspace_certificates"
+  value       = { for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : k => v.expiration }
 }
 output "api_management_workspace_certificates_key_vault_secret_id" {
-  description = "List of key_vault_secret_id values across all api_management_workspace_certificates"
-  value       = [for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : v.key_vault_secret_id]
+  description = "Map of key_vault_secret_id values across all api_management_workspace_certificates, keyed the same as var.api_management_workspace_certificates"
+  value       = { for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : k => v.key_vault_secret_id }
 }
 output "api_management_workspace_certificates_name" {
-  description = "List of name values across all api_management_workspace_certificates"
-  value       = [for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : v.name]
+  description = "Map of name values across all api_management_workspace_certificates, keyed the same as var.api_management_workspace_certificates"
+  value       = { for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : k => v.name }
 }
 output "api_management_workspace_certificates_password" {
-  description = "List of password values across all api_management_workspace_certificates"
-  value       = [for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : v.password]
+  description = "Map of password values across all api_management_workspace_certificates, keyed the same as var.api_management_workspace_certificates"
+  value       = { for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : k => v.password }
   sensitive   = true
 }
 output "api_management_workspace_certificates_subject" {
-  description = "List of subject values across all api_management_workspace_certificates"
-  value       = [for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : v.subject]
+  description = "Map of subject values across all api_management_workspace_certificates, keyed the same as var.api_management_workspace_certificates"
+  value       = { for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : k => v.subject }
 }
 output "api_management_workspace_certificates_thumbprint" {
-  description = "List of thumbprint values across all api_management_workspace_certificates"
-  value       = [for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : v.thumbprint]
+  description = "Map of thumbprint values across all api_management_workspace_certificates, keyed the same as var.api_management_workspace_certificates"
+  value       = { for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : k => v.thumbprint }
 }
 output "api_management_workspace_certificates_user_assigned_identity_client_id" {
-  description = "List of user_assigned_identity_client_id values across all api_management_workspace_certificates"
-  value       = [for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : v.user_assigned_identity_client_id]
+  description = "Map of user_assigned_identity_client_id values across all api_management_workspace_certificates, keyed the same as var.api_management_workspace_certificates"
+  value       = { for k, v in azurerm_api_management_workspace_certificate.api_management_workspace_certificates : k => v.user_assigned_identity_client_id }
 }
 
